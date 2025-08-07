9d0f7e0ec92e680940e575b0e361e631f61062ac
rhktkd <84678409+rhktkd@users.noreply.github.com>
2025-08-07 17:17:13 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path');
 const app = express()
-const port = 80
+const port = 3000
 var iconv = require('iconv-lite');
 const fetch = require('node-fetch'); // node-fetch 라이브러리가 필요합니다.
 
